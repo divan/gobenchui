30c07e37fc08fb3474ffccd7109ee5bbc4aa350a
Ivan Daniluk <ivan.daniluk@gmail.com>
2015-10-17 20:33:22 -0400

    Add filter max function

--- filter.go
+++ filter.go
@@ -23,3 +23,41 @@ func NewFilterOptions(lastN, max int64, args ...string) *FilterOptions {
 		Args:  args,
 	}
 }
+
+// FilterMax filters commits, selecting at most 'max'
+// evenly distributed items.
+//
+// Idea is to get brief overview of benchmark progress
+// overtime for codebases with large number of commits.
+// If there are 10K commits and max=100, it will pick
+// 100 commits from very beginning to the last one, with
+// the equal time intervals between each picked commit.
+func FilterMax(commits []Commit, max int64) []Commit {
+	// special cases
+	if max < 2 {
+		return commits
+	}
+	length := int64(len(commits))
+	if max >= length || length == 0 {
+		return commits
+	}
+
+	// rough naive implementation (by commits number)
+	var ret []Commit
+	for i := int64(0); i < length; {
+		// how many items left in target slice
+		left := max - int64(len(ret)) - 1
+		if left == 0 {
+			left = 1
+		}
+		// size of next step
+		size := (length - i - 1) / left
+		if size == 0 {
+			size = 1
+		}
+
+		ret = append(ret, commits[i])
+		i = i + size
+	}
+	return ret
+}
---
+++ filter_test.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+	"time"
+)
+
+func TestFilterMax(t *testing.T) {
+	Convey("Filter Max should filter commits evenly", t, func() {
+		N := int64(100)
+		var commits []Commit
+		for i := int64(0); i < N; i++ {
+			date, _ := time.Parse("2006-01-02 15:04:05", fmt.Sprintf("2015-%02d-%02d 18:%02d:01", i%12+1, i%28+1, i%60))
+			commit := Commit{
+				Hash:    fmt.Sprintf("Hash %d", i),
+				Author:  fmt.Sprintf("Author %d", i%3),
+				Subject: fmt.Sprintf("Subject %d", i),
+				Date:    date,
+			}
+			commits = append(commits, commit)
+		}
+		Convey("Special case, len == 0", func() {
+			res := FilterMax([]Commit{}, 0)
+			So(len(res), ShouldEqual, 0)
+		})
+		Convey("Special case, max > len", func() {
+			res := FilterMax(commits, N*2)
+			So(len(res), ShouldEqual, N)
+		})
+		Convey("Special case, max == 0", func() {
+			res := FilterMax(commits, 0)
+			So(len(res), ShouldEqual, N)
+		})
+		Convey("Max = 2, start and end", func() {
+			res := FilterMax(commits, 2)
+			So(len(res), ShouldEqual, 2)
+			So(res[0].Hash, ShouldEqual, "Hash 0")
+			So(res[1].Hash, ShouldEqual, "Hash 99")
+		})
+		Convey("Max = 3, start, middle and end", func() {
+			res := FilterMax(commits, 3)
+			So(len(res), ShouldEqual, 3)
+			So(res[0].Hash, ShouldEqual, "Hash 0")
+			So(res[1].Hash, ShouldEqual, "Hash 49")
+			So(res[2].Hash, ShouldEqual, "Hash 99")
+		})
+		Convey("Max = 10, tens", func() {
+			res := FilterMax(commits, 10)
+			So(len(res), ShouldEqual, 10)
+			So(res[0].Hash, ShouldEqual, "Hash 0")
+			So(res[1].Hash, ShouldEqual, "Hash 11")
+			So(res[2].Hash, ShouldEqual, "Hash 22")
+			So(res[3].Hash, ShouldEqual, "Hash 33")
+			So(res[4].Hash, ShouldEqual, "Hash 44")
+			So(res[5].Hash, ShouldEqual, "Hash 55")
+			So(res[6].Hash, ShouldEqual, "Hash 66")
+			So(res[7].Hash, ShouldEqual, "Hash 77")
+			So(res[8].Hash, ShouldEqual, "Hash 88")
+			So(res[9].Hash, ShouldEqual, "Hash 99")
+		})
+		Convey("Max = 98, near len", func() {
+			res := FilterMax(commits, 98)
+			So(len(res), ShouldEqual, 98)
+			So(res[0].Hash, ShouldEqual, "Hash 0")
+			So(res[90].Hash, ShouldEqual, "Hash 90")
+			So(res[96].Hash, ShouldEqual, "Hash 97")
+			So(res[97].Hash, ShouldEqual, "Hash 99")
+		})
+		Convey("Max = 75, 3/4", func() {
+			res := FilterMax(commits, 75)
+			So(len(res), ShouldEqual, 75)
+			So(res[0].Hash, ShouldEqual, "Hash 0")
+			So(res[1].Hash, ShouldEqual, "Hash 1")
+			So(res[73].Hash, ShouldEqual, "Hash 97")
+			So(res[74].Hash, ShouldEqual, "Hash 99")
+		})
+	})
+}
